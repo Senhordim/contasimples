2ef9d73a05e505c3c477557bdc2116af4aa6480d
Diego Collares <diego.collares@gmail.com>
2023-11-16 10:18:43 -0300

    create route and namespace admin with controllers

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,16 @@
 Rails.application.routes.draw do
+  get 'landing/index'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
   # Can be used by load balancers and uptime monitors to verify that the app is live.
-  get "up" => "rails/health#show", as: :rails_health_check
+  get 'up' => 'rails/health#show', as: :rails_health_check
 
   # Defines the root path route ("/")
-  # root "posts#index"
+  root 'landing#index'
+
+  namespace :admin do
+    get 'dashboard/index'
+    root 'dashboard#index'
+  end
 end
