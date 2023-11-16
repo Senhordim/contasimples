da99770cb241fa1034ff06cdf237aa996e19d804
Diego Collares <diego.collares@gmail.com>
2023-11-16 10:48:19 -0300

    create auth and lint with rubocop

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,18 @@
+# frozen_string_literal: true
+
 Rails.application.routes.draw do
+  get  'sign_in', to: 'sessions#new'
+  post 'sign_in', to: 'sessions#create'
+  get  'sign_up', to: 'registrations#new'
+  post 'sign_up', to: 'registrations#create'
+  resources :sessions, only: %i[index show destroy]
+  resource  :password, only: %i[edit update]
+  namespace :identity do
+    resource :email,              only: %i[edit update]
+    resource :email_verification, only: %i[show create]
+    resource :password_reset,     only: %i[new edit create update]
+  end
+
   get 'landing/index'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
