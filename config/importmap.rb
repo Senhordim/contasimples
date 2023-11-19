6166efd3e77f6c61d29fb955f861ac463551250e
Diego Collares <diego.collares@gmail.com>
2023-11-19 10:20:43 -0300

    first structure admin template css and js base

--- config/importmap.rb
+++ config/importmap.rb
@@ -1,9 +1,12 @@
-# frozen_string_literal: true
-
 # Pin npm packages by running ./bin/importmap
-
 pin 'application', preload: true
+
+pin 'alpinejs', to: 'https://ga.jspm.io/npm:alpinejs@3.13.2/dist/module.esm.js'
+pin 'alpine-turbo-drive-adapter', to: 'https://ga.jspm.io/npm:alpine-turbo-drive-adapter@2.1.0/dist/alpine-turbo-drive-adapter.esm.js'
+pin '@alpinejs/persist', to: 'https://ga.jspm.io/npm:@alpinejs/persist@3.13.2/dist/module.esm.js'
+
 pin '@hotwired/turbo-rails', to: 'turbo.min.js', preload: true
 pin '@hotwired/stimulus', to: 'stimulus.min.js', preload: true
 pin '@hotwired/stimulus-loading', to: 'stimulus-loading.js', preload: true
+
 pin_all_from 'app/javascript/controllers', under: 'controllers'
